c84ffc3300f546108bf59e52d4e39d7dfcf50dde
Oskarlacz <131953273+ymamahama@users.noreply.github.com>
2024-02-27 20:46:57 +0100

    add function

--- app/src/main/java/com/example/oskarlaczmanski/MainActivity.java
+++ app/src/main/java/com/example/oskarlaczmanski/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.oskarlaczmanski;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.changeText);
+                textView.setText("Start");
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.changeText);
+                textView.setText("Oskar Łaczmański");
+            }
+        });
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.changeText);
+                textView.setText("Good Bye");
+            }
+        });
     }
+
 }
